be9a5592d152d8e1f908cdc3b3218eaa2befc436
Keionne Derousselle <knnderousselle@gmail.com>
2020-03-30 20:51:24 -0600

    learning about function binding and react hooks

--- src/App.js
+++ src/App.js
@@ -1,30 +1,72 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import Person from './Person/Person'
 
-class App extends Component {
-  state = {
-    persons: [ { name: 'Keionne', age: 25 }, { name: 'Darrius', age: 25 }, { name: 'Lynus', age: 5 } ]
-  }
+// CLASS BASED COMPONENT
+// class App extends Component {
+//   state = {
+//     persons: [ { name: 'Keionne', age: 25 }, { name: 'Darrius', age: 25 }, { name: 'Lynus', age: 5 } ]
+//   }
 
-  switchNameHandler = () => {
-    this.setState({
-      persons: this.state.persons.map(p => ({
-        name: p.name,
+//   switchNameHandler = () => {
+//     this.setState({
+//       persons: this.state.persons.map(p => ({
+//         name: p.name,
+//         age: Math.floor(Math.random() * 100)
+//       }))
+//     })
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//         <button onClick={this.switchNameHandler}>Switch Name</button>
+//         <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+//         <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: Fishing!</Person>
+//         <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+//       </div>
+//     )
+//   }
+// }
+
+// REACT HOOKS
+const app = () => {
+  const [ personsState, setPersonsState ] = useState({
+    persons: [ { name: 'Keionne', age: 25 }, { name: 'Darrius', age: 25 }, { name: 'Lynus', age: 5 } ]
+  })
+
+  // const [ otherState, setOtherState ] = useState({
+  //   other: 'some other value'
+  // })
+
+    const switchNameHandler = newName => {
+    setPersonsState({
+      persons: personsState.persons.map(() => ({
+        name: newName,
         age: Math.floor(Math.random() * 100)
       }))
     })
   }
+  // The bind syntax is more performant than anonymous function calls
 
-  render() {
-    return (
-      <div>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: Fishing!</Person>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
-      </div>
-    )
-  }
+  return (
+    <div>
+      <button onClick={switchNameHandler}>Switch Name</button>
+      <Person
+        name={personsState.persons[0].name}
+        age={personsState.persons[0].age}
+        click={() => switchNameHandler('Keionne')} />
+      <Person
+        name={personsState.persons[1].name}
+        age={personsState.persons[1].age}
+        click={switchNameHandler.bind(this, 'Darrius')}>
+          My Hobbies: Fishing!
+      </Person>
+      <Person
+        name={personsState.persons[2].name}
+        age={personsState.persons[2].age}
+        click={() => switchNameHandler('Lynus')} />
+    </div>
+  )
 }
 
-export default App
+export default app
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,9 +1,11 @@
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
 /* eslint-disable react/prop-types */
 import React from 'react'
 
-const person = ({ name, age, children }) => (
+const person = ({ name, age, children, click }) => (
     <div>
-      <p>I&apos;m { name } and I am { age } years old!</p>
+      <p onClick={click}>I&apos;m { name } and I am { age } years old!</p>
       <p>{ children }</p>
     </div>
   )
